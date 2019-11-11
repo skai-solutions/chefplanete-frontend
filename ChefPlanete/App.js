660ce7defed373a9eba91fa6ce1d3722a9a82d06
ashinit <ashini.tennakoon@gmail.com>
2019-11-11 15:54:57 -0500

    Updated

--- ChefPlanete/App.js
+++ ChefPlanete/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-import GoalItem from './Screens/Start';
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Start: {screen: StartScreen},
+});
 
-export default function App() {
-  return (
-    <View >
-      <Text> hello </Text>
-    </View>
-  );
-}
+const App = createAppContainer(MainNavigator);
 
+export default App;
 
