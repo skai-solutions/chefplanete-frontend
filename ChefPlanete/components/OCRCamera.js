1968a0fdddcf43ddfb723601a499636f06a9f531
Kahlia <klepple@uwo.ca>
2019-11-11 15:42:17 -0500

    OCRCamera still doesn't work but it works more!

--- ChefPlanete/components/OCRCamera.js
+++ ChefPlanete/components/OCRCamera.js
@@ -1,54 +1,54 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Button } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
 const OCRCamera = props => {
   const [cameraPermission, setCameraPermission] = useState(null);
 
-  const componentDidMount = () => {
+  const componentDidMount = async() => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     setCameraPermission(() => {
       return status === 'granted';
     });
   }
 
-  if (cameraPermission === null) {
-    return <View />;
-  } else if (cameraPermission === false) {
-    return <Text>No access to camera</Text>;
-  } else {
-    return (
-      <View style={{ flex: 1 }}>
-        <Button title="Set camera permission" onPress={componentDidMount}/>
-        <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back}>
-          <View
+  // if (cameraPermission === null) {
+  //   return <View />;
+  // } else if (cameraPermission === false) {
+  //   return <Text>No access to camera</Text>;
+  // } else {
+  return (
+    <View style={{ flex: 1 }}>
+      <Button title="Set camera permission" onPress={componentDidMount}/>
+      <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
             style={{
-              flex: 1,
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              this.setState({
+                type:
+                  this.state.type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back,
+              });
             }}>
-            <TouchableOpacity
-              style={{
-                flex: 0.1,
-                alignSelf: 'flex-end',
-                alignItems: 'center',
-              }}
-              onPress={() => {
-                this.setState({
-                  type:
-                    this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                });
-              }}>
-              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-            </TouchableOpacity>
-          </View>
-        </Camera>
-      </View>
-    );
-  }
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+  // }
 }
 
 export default OCRCamera;
