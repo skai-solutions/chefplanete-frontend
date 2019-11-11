b6970629a11b4ce08f1ed837601c5d08673097ad
Kahlia <klepple@uwo.ca>
2019-11-11 15:32:42 -0500

    Add OCRCamera component (it doesn't work tho)

--- ChefPlanete/App.js
+++ ChefPlanete/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import OCRCamera from "./components/OCRCamera"
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <OCRCamera></OCRCamera>
     </View>
   );
 }
---
+++ ChefPlanete/components/OCRCamera.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+
+const OCRCamera = props => {
+  const [cameraPermission, setCameraPermission] = useState(null);
+
+  const componentDidMount = () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    setCameraPermission(() => {
+      return status === 'granted';
+    });
+  }
+
+  if (cameraPermission === null) {
+    return <View />;
+  } else if (cameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  } else {
+    return (
+      <View style={{ flex: 1 }}>
+        <Button title="Set camera permission" onPress={componentDidMount}/>
+        <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+              }}
+              onPress={() => {
+                this.setState({
+                  type:
+                    this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                });
+              }}>
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    );
+  }
+}
+
+export default OCRCamera;
+
+// export default class CameraExample extends React.Component {
+//   state = {
+//     hasCameraPermission: null,
+//     type: Camera.Constants.Type.back,
+//   };
+
+//   async componentDidMount() {
+//     const { status } = await Permissions.askAsync(Permissions.CAMERA);
+//     this.setState({ hasCameraPermission: status === 'granted' });
+//   }
+
+//   render() {
+//     const { hasCameraPermission } = this.state;
+//     if (hasCameraPermission === null) {
+//       return <View />;
+//     } else if (hasCameraPermission === false) {
+//       return <Text>No access to camera</Text>;
+//     } else {
+//       return (
+//         <View style={{ flex: 1 }}>
+//           <Camera style={{ flex: 1 }} type={this.state.type}>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 backgroundColor: 'transparent',
+//                 flexDirection: 'row',
+//               }}>
+//               <TouchableOpacity
+//                 style={{
+//                   flex: 0.1,
+//                   alignSelf: 'flex-end',
+//                   alignItems: 'center',
+//                 }}
+//                 onPress={() => {
+//                   this.setState({
+//                     type:
+//                       this.state.type === Camera.Constants.Type.back
+//                         ? Camera.Constants.Type.front
+//                         : Camera.Constants.Type.back,
+//                   });
+//                 }}>
+//                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+//               </TouchableOpacity>
+//             </View>
+//           </Camera>
+//         </View>
+//       );
+//     }
+//   }
+// }
