78ddc983c3d8b69b52f06b251504cd5c4576fd4f
Steven Li <steven.zelin.li@gmail.com>
2020-02-26 21:21:50 -0500

    Fixed pantry reducer for updating pantry

--- ChefPlanete/reducers/pantry.js
+++ ChefPlanete/reducers/pantry.js
@@ -1,27 +1,38 @@
 import actionTypes from '../actions/actionTypes';
 
 const DEFAULT_STATE = {
-    loading: false,
-    data: {},
-    errors: null
+  loading: false,
+  data: {},
+  errors: null
 };
 
 export default (state = DEFAULT_STATE, action) => {
-    switch (action.type) {
-        case actionTypes.FETCH_PANTRY_STARTED:
-        case actionTypes.UPDATE_PANTRY_STARTED:
-            return {...state, loading: true};
-        case actionTypes.FETCH_PANTRY_FAILURE:
-        case actionTypes.UPDATE_PANTRY_FAILURE:
-            return {...state, loading: false, errors: action.payload};
-        case actionTypes.FETCH_PANTRY_SUCCESS:
-            return {...state, loading: false, data: action.payload};
-        case actionTypes.UPDATE_PANTRY_SUCCESS:
-            // We simple update the original profile with the new fields
-            return {...state, loading: false, data: {...state.data, ...action.payload}};
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.FETCH_PANTRY_STARTED:
+    case actionTypes.UPDATE_PANTRY_STARTED:
+      return {...state, loading: true};
+    case actionTypes.FETCH_PANTRY_FAILURE:
+    case actionTypes.UPDATE_PANTRY_FAILURE:
+      return {...state, loading: false, errors: action.payload};
+    case actionTypes.FETCH_PANTRY_SUCCESS:
+      return {...state, loading: false, data: action.payload};
+    case actionTypes.UPDATE_PANTRY_SUCCESS:
+      // We simple update the original profile with the new fields
+      let updatedPantry = {...state.data};
+      Object.entries(action.payload).forEach(([key, value]) => {
+        if (value.quantity > 0) {
+          updatedPantry[key] = value;
+        }
+        else {
+          if (updatedPantry[key]) {
+            delete updatedPantry[key];
+          }
+        }
+      });
+      return {...state, loading: false, data: updatedPantry};
+    default:
+      return state;
+  }
 }
 
 /**
@@ -30,24 +41,24 @@ export default (state = DEFAULT_STATE, action) => {
  */
 
 export const getPantry = (state) => {
-    if (!state || !state.data) {
-        throw new Error("No Pantry data found. Is user signed in?")
-    }
-    return state.data;
+  if (!state || !state.data) {
+    throw new Error("No Pantry data found. Is user signed in?")
+  }
+  return state.data;
 };
 
 export const isLoading = (state) => {
-    if (!state) {
-        throw new Error("No Pantry data found.")
-    }
-    return state.loading;
+  if (!state) {
+    throw new Error("No Pantry data found.")
+  }
+  return state.loading;
 };
 
 export const getErrors = (state) => {
-    if (!state) {
-        throw new Error("No Pantry data found.")
-    } else if (!state.errors) {
-        return null;
-    }
-    return state.errors.message;
+  if (!state) {
+    throw new Error("No Pantry data found.")
+  } else if (!state.errors) {
+    return null;
+  }
+  return state.errors.message;
 };
