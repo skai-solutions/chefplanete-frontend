9c6ab613f9be2643472a13c37aec7af8ef08043a
Kahlia <klepple@uwo.ca>
2020-03-05 16:43:56 -0500

    Display data properly

--- ChefPlanete/Screens/Profile.js
+++ ChefPlanete/Screens/Profile.js
@@ -13,6 +13,8 @@ import {
   Picker,
   Row,
   Title,
+  List,
+  ListItem,
   Input,
   Text
 } from "native-base"
@@ -38,84 +40,64 @@ const Profile = ({ onSubmit, dietaryProfile, loading, errors, navigation }) => {
   const [goalsCompleted, setGoalsCompleted] = useState(dietaryProfile.totalGoalsCompleted);
   const [foodRestrictions, setFoodRestrictions] = useState(dietaryProfile.foodRestrictions);
 
-  const updateCookingLevel = (cookingLevel) => {
-    // if (cookingLevel && cookingLevel !== "") {
-    //   cookingLevel = parseFloat(cookingLevel);
-    // } else {
-    //   cookingLevel = 0;
-    // }
-    console.log("updating cooking level: ", cookingLevel);
-    setCookingLevel(cookingLevel);
-  };
-
-  const updateGoalsCompleted = (goalsCompleted) => {
-    // if (goalsCompleted && goalsCompleted !== "") {
-    //   goalsCompleted = parseFloat(goalsCompleted);
-    // } else {
-    //   goalsCompleted = 0;
-    // }
-    console.log("updating goals completed: ", cookingLevel);
-    setGoalsCompleted(goalsCompleted);
-  };
-
   return (
     <Container style={styles.container}>
       <PageHeader title="Profile"/>
-      <Content>
-        {
-          !loading ?
-          <View>
-            <Picker
-              iosIcon={<Icon style={{color: "black"}} name="arrow-down" />}
-              mode="dropdown"
-              selectedValue={cookingLevel}
-              placeholderIconColor="white"
-              onValueChange={(itemValue) => updateCookingLevel(itemValue, key)}
-            >
-            <Picker.Item label="1 -Beginner" value="1" />
-              <Picker.Item label="2 - Intermediate" value="2" />
-              <Picker.Item label="3 - Advanced" value="3" />
-            </Picker>
-            <Input
-              keyboardType="decimal-pad"
-              onChangeText={(newText) => updateGoalsCompleted(newText)}
-              value={goalsCompleted}
-            />
-            {
-              Object.entries(foodRestrictions).sort(([keyA], [keyB]) => {
-                if (keyA > keyB) return -1;
-                if (keyB > keyA) return 1;
-                return 0;
-              }).map(([key, value]) => {
-                return (
-                  <Card style={styles.card} key={key}>
-                    <CardItem style={styles.card}>
-                      <Body style={styles.cardItems}>
-                        <Item style={{flex: 4}} underline>
-                          <Input
-                            onChangeText={(newText) => updateCookingLevel(newText)}
-                            value={foodRestrictions}
-                          />
-                        </Item>
-                      </Body>
-                    </CardItem>
-                  </Card>
-                );
-              })
-            }
-            <Button onPress={() => navigate("ManageProfile")} style={{height: "90%"}}>
-                  <Text adjustsFontSizeToFit>Manage</Text>
-            </Button>
-          </View> : <Text adjustsFontSizeToFit style={styles.heading}>Unable to find profile data associated with this user.</Text>
-        }
-        {
-          isErrorState &&
-          <View>
-            <Text style={styles.text}>Error(s) loading the user's profile:</Text>
-            <Text style={styles.errorText}>{errors}</Text>
-          </View>
-        }
-      </Content>
+        <Content>
+          {
+            !loading ?
+            <View>
+              <Card transparent style={styles.card}>
+                <CardItem style={styles.card} header>
+                  <Content>
+                    <View style={{justifyContent: "space-between", flexDirection: "row"}}>
+                      <Text style={styles.heading}>Total Goals Completed</Text>
+                      <Text style={styles.heading}>{goalsCompleted}</Text>
+                    </View>
+                  </Content>
+                </CardItem>
+              </Card>
+              <Card transparent style={styles.card}>
+                <CardItem style={styles.card} header>
+                  <Content>
+                    <View style={{justifyContent: "space-between", flexDirection: "row"}}>
+                      <Text style={styles.heading}>Cooking Level</Text>
+                      <Text style={styles.heading}>{cookingLevel}</Text>
+                    </View>
+                  </Content>
+                </CardItem>
+              </Card>
+              <Card transparent style={styles.card}>
+                <CardItem style={styles.card} header>
+                  <Content>
+                    <Text style={styles.heading}>Dietary Restrictions</Text>
+                    <List style={{padding: 0, margin: 0}}>
+                      {
+                        foodRestrictions.map(restriction =>
+                          <ListItem button style={styles.restrictions}>
+                            <Body>
+                              <Text>{restriction}</Text>
+                            </Body>
+                          </ListItem>
+                        )
+                      }
+                    </List>
+                  </Content>
+                </CardItem>
+              </Card>
+              <Button onPress={() => navigate("ManageProfile")}>
+                <Text adjustsFontSizeToFit>Manage</Text>
+              </Button>
+            </View> : <Text adjustsFontSizeToFit style={styles.heading}>Unable to find profile data associated with this user.</Text>
+          }
+          {
+            isErrorState &&
+            <View>
+              <Text style={styles.text}>Error(s) loading the user's profile:</Text>
+              <Text style={styles.errorText}>{errors}</Text>
+            </View>
+          }
+        </Content>
       <NavigationBar currentScreen="PROFILE"/>
     </Container>
   );
@@ -130,10 +112,17 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     backgroundColor: StyleVars.cardBackground,
   },
-  cardItems: {
-    paddingTop: "5%",
-    paddingLeft: "5%",
-    paddingRight: "5%",
+  heading: {
+    fontFamily: "SF Pro Display Heavy",
+    fontSize: 20,
+    color: StyleVars.headingColor,
+  },
+  restrictions: {
+    width: '100%',
+    marginLeft: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    marginRight: 0
   },
   text: {
     color: "rgb(0,0,0)",
